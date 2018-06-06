ee232d566d44693c2bcafab7c6885f0fd855edb8
varad-25 <vrdkulkarni@gmail.com>
2018-06-06 16:40:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to JAVA");
+        System.out.println("Welcome");
     }
 }    
         
