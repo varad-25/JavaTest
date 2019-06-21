38f8d3285501639a1246a64370fbe6df01647a50
varad-25 <vrdkulkarni@gmail.com>
2019-06-21 16:53:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         public HelloWorld(int a, int b) {
-            System.out.println("Adding of (a + b) = "+ (a+b));\
+            System.out.println("Adding of (a + b) = "+ (a+b));
         }
         HelloWorld h = new HelloWorld(10,20);
     }
